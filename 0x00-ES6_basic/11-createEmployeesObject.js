5c503e9dcd4a2e0340e5231ed218755b14ea5b17
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-21 08:26:57 +0100

    refactor task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const obj = {};
-  obj[departmentName] = [...employees];
-  return obj;
+  return {
+    [departmentName]: [
+      ...employees,
+    ],
+  };
 }
