0667d7e606d852df6a7ae1ea89d379190ede7032
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-27 22:56:44 +0100

    fix task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,15 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 const updateStudentGradeByCity = (students, city, newGrades) => {
   if (!Array.isArray(students)) {
     return [];
   }
-  const studentsByLocation = getStudentsByLocation(students, city);
-  const updatedStudents = studentsByLocation.map((student) => {
-    const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
-    if (studentGrade) {
-      return { ...student, grade: studentGrade.grade };
-    }
-    return { ...student, grade: 'N/A' };
-  });
-  return updatedStudents;
+  return students.filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : 'N/A',
+      };
+    });
 };
 
 export default updateStudentGradeByCity;
