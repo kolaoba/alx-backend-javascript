66af2a5be86ec3cd8f0bd68e5da8a90f2c40b41e
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-27 21:30:38 +0100

    run task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+const getListStudentsIds = (students) => {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  const studentIds = students.map((student) => student.id);
+  return studentIds;
+};
+
+export default getListStudentsIds;
