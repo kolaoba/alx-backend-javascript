68749669fc55a0a399c01eea6460b0eceae9c681
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-28 22:49:36 +0100

    fix task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,13 @@
-const updateUniqItems = (items) => items.forEach((item, key, map) => {
-  if (item === 1) {
-    map.set(key, 100);
+const updateUniqItems = (items) => {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
   }
-  return item;
-});
+  items.forEach((item, key, map) => {
+    if (item === 1) {
+      map.set(key, 100);
+    }
+    return item;
+  });
+};
 
 export default updateUniqItems;
