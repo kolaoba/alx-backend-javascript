e2419efbb3d354185d3f1c617ceb94f9d5819bd3
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-05-21 21:08:32 +0100

    add task 10

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,17 +1,22 @@
-const express = require('express');
-
-const app = express();
+const express = require("express");
 const port = 7865;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.send("Welcome to the payment system");
 });
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.send(`Payment methods for cart ${req.params.id}`);
+app.get("/cart/:id([0-9]+)", (req, res) => {
+  const id = req.params.id;
+  if (isNaN(id)) {
+    res.statusCode = 404;
+    res.send("Not a number");
+  } else {
+    res.statusCode = 200;
+    res.send(`Payment methods for cart ${id}`);
+  }
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
-module.exports = app;
+app.listen(port, () => console.log("API available on localhost port 7865"));
